7f6ee68b44b5fd1c2b53e1c094c6eef51b3ea207
Akshay krishna <84134962+AkshaykrishnaM@users.noreply.github.com>
2023-10-16 01:11:52 +0800

    Create First.cpp

---
+++ First.cpp
@@ -0,0 +1,31 @@
+/*
+NAME: AKSHAY KRISHNA
+STUDENT ID: 0137246
+SEMESTER: 1
+LECTURER: MS. BRIDGET
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int Nos = 1;
+    float Price;
+    double quantity;
+    char size_code;
+    string description;
+    bool foam = true;
+
+    Price = 45.00;
+    quantity = 1.5;
+    size_code = 'L';
+    description = "Layer cakes and cupcakesare specially packaged  They will remain fresh after arrival for 4 days.Please Enjoy.";
+    foam = 5;
+
+    cout << "The price of the cake is: " << Price << "RM" << endl;
+    cout << "Quantity : " << quantity << endl;
+    cout << "Size code : " << size_code  << endl;
+    cout << "Disclaimer : " << description << endl;
+    cout << "foam quantity: " << foam << endl;
+}
