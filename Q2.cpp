7afc8d5a53460972d63e99a73ad1cb491094840f
Akshay krishna <84134962+AkshaykrishnaM@users.noreply.github.com>
2023-10-15 21:25:26 +0800

    Create Q2.cpp
    Weight measuring for POS Malaysia

---
+++ Q2.cpp
@@ -0,0 +1,38 @@
+/*
+
+NAME: AKSHAY KRISHNA
+STUDENT ID: 0137246
+SEMESTER: 1
+LECTURER: MS. BRIDGET 
+
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    double weight;
+
+    std::cout << "Enter parcel weight (Kg): ";
+    std::cin >> weight;
+
+    if (weight >= 0)
+        if (weight < 2.5)
+            cout << "Delivery Charges is RM" << weight * 3.50 << endl;
+
+        else
+            if (weight <= 5)
+                cout << "Delivery Charges is RM" << weight * 2.85 << endl;
+
+            else
+                cout << "Delivery Charges is RM" << weight * 2.45 << endl;
+
+
+
+    else
+        cout << "Weight must be above zero" << endl;
+
+
+    
+
+}
